489c9f05bd4fcfac33893a20c4da46d91427ada6
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-02-21 14:19:12 +0100

    Feat: db initialization
    Add db context, a design time db context factory, neccesary packages and connection string to appsettings.json, create initial migration with base Identity Framework user models

---
+++ EntityModelLibrary/DbContext.cs
@@ -0,0 +1,32 @@
+﻿using EntityModelLibrary.Models.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityModelLibrary
+{
+    internal class DbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        // for checking that DI is getting a different instance each time when the dbcontext is injected in the context of a web request
+        private Guid _instanceId = Guid.NewGuid();
+
+        public static void AddBaseOptions(DbContextOptionsBuilder<DbContext> builder, string connectionString)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException("Connection string must be provided", nameof(connectionString));
+
+            builder.UseSqlServer(connectionString, x => { x.EnableRetryOnFailure(); });
+        }
+    }
+}
---
+++ EntityModelLibrary/DesignTimeDBContextFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace EntityModelLibrary
+{
+    internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DbContext>
+    {
+        public DbContext CreateDbContext(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            IConfigurationBuilder builder =
+                new ConfigurationBuilder()
+                    .SetBasePath(path)
+                    .AddJsonFile("appsettings.json");
+
+            IConfigurationRoot config = builder.Build();
+
+            string connectionString = config.GetConnectionString("ConnString");
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("Could not find connection string named 'ConnString'");
+            }
+
+            DbContextOptionsBuilder<DbContext> dbContextOptionsBuilder =
+                new DbContextOptionsBuilder<DbContext>();
+
+            DbContext.AddBaseOptions(dbContextOptionsBuilder, connectionString);
+
+            return new DbContext(dbContextOptionsBuilder.Options);
+        }
+    }
+}
