5147d34f6c31d494d3a1954ca3dbd6f955920cd8
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-02-23 18:03:38 +0100

    Add: security util tests

--- AuthServiceTests/SecurityUtilTests.cs
+++ AuthServiceTests/SecurityUtilTests.cs
@@ -16,12 +16,37 @@ namespace AuthServiceTests
         public void VerifyPassword_MatchingPasswords_ReturnsTrue()
         {
             // Arrange 
-            string password = "Jolan";
+            string password = "pw";
             string salt = _securityUtil.CreateSalt();
             string hashedPassword = _securityUtil.HashPassword(password, salt);
 
             // Assert
             Assert.IsTrue(_securityUtil.VerifyPassword(password, salt, hashedPassword));
+        }
+
+        [Test]
+        public void HashPassword_ValidInput_TransformsString()
+        {
+            // Arrange
+            string password = "pw";
+            string salt = _securityUtil.CreateSalt();
+            string hashedPassword = _securityUtil.HashPassword(password, salt);
+
+            Assert.That(password, Is.Not.EqualTo(hashedPassword));
+        }
+
+        [Test]
+        public void HashPassword_DifferentSalts_outputDiffers()
+        {
+            // Arrange
+            string password = "pw";
+            string salt1 = _securityUtil.CreateSalt();
+            string hashedPassword1 = _securityUtil.HashPassword(password, salt1);
+
+            string salt2 = _securityUtil.CreateSalt();
+            string hashedPassword2 = _securityUtil.HashPassword(password, salt2);
+
+            Assert.That(hashedPassword1, Is.Not.EqualTo(hashedPassword2));
         }
         
     }
