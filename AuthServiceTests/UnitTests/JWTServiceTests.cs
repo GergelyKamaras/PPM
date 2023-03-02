91ae6347e539562ce473adc977025559cfafd8b5
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-03-02 13:24:03 +0100

    Add: JWTService unit tests

---
+++ AuthServiceTests/UnitTests/JWTServiceTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Mime;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AuthService.Authentication;
+using AuthServiceModelLibrary.ApplicationUser;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthServiceTests.UnitTests
+{
+    internal class JWTServiceTests
+    {
+        private IJWTService _service;
+        private ApplicationUser _user;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            IConfiguration config = new ConfigurationManager();
+            config["JWT:ValidIssuer"] = "TestIssuer";
+            config["JWT:ValidAudience"] = "TestAudience";
+            config["JWT:Secret"] = "SuperSecret";
+            _service = new JWTService(config);
+
+            _user = new ApplicationUser()
+            {
+                Id = "123",
+                FirstName = "Jolan",
+                LastName = "Hegyi",
+                UserName = "Hegyine",
+                Email = "hegyine@hegy.com",
+                PasswordHash = "pw",
+                Salt = "123",
+                Role = "Administrator"
+            };
+        }
+
+        [Test]
+        public void GenerateLoginJWT_ValidUser_IdsMatch()
+        {
+            // Act
+            JwtSecurityToken token = _service.GenerateLoginJWT(_user); 
+
+            // Assert
+            Assert.That(new Claim("Id", _user.Id).Value, 
+                Is.EqualTo(token.Claims.First(c => c.Type == "Id").Value));
+        }
+
+        [Test]
+        public void GenerateLoginJWT_ValidUser_RolesMatch()
+        {
+            // Act
+            JwtSecurityToken token = _service.GenerateLoginJWT(_user);
+
+            // Assert
+            Assert.That(token.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role).Value,
+                Is.SameAs(new Claim(ClaimTypes.Role, _user.Role).Value));
+        }
+
+        [Test]
+        public void GenerateLoginJWT_ValidUser_EmailsMatch()
+        {
+            // Act
+            JwtSecurityToken token = _service.GenerateLoginJWT(_user);
+
+            // Assert
+            Assert.That(token.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value,
+                Is.EqualTo(new Claim(ClaimTypes.Email, _user.Email).Value));
+        }
+
+        [Test]
+        public void GenerateLoginJWT_ValidUser_UsernamesMatch()
+        {
+            // Act
+            JwtSecurityToken token = _service.GenerateLoginJWT(_user);
+            
+            // Assert
+            Assert.That(token.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name).Value, 
+                Is.EqualTo(new Claim(ClaimTypes.Name, _user.UserName).Value));
+        }
+
+        [Test]
+        public void GenerateLoginJWT_InvalidUser_ThrowsError()
+        {
+            // Arrange
+            ApplicationUser user = new ApplicationUser();
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => _service.GenerateLoginJWT(user));
+
+        }
+    }
+}
