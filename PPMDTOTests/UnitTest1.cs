06c74b1734f703e2ee069c81f8118ecb28cf8f40
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-03-07 11:23:23 +0100

    Add: new project skeletons for PPMAPI service

---
+++ PPMDTOTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PPMDTOTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
