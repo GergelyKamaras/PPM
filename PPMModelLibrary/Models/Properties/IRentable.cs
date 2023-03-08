d1b8b1eaee0faae855b17a27f54cf83e2f3e0500
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-03-08 10:21:08 +0100

    Refactor: remove unused using statements

--- PPMModelLibrary/Models/Properties/IRentable.cs
+++ PPMModelLibrary/Models/Properties/IRentable.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PPMModelLibrary.Models.Users;
+﻿using PPMModelLibrary.Models.Users;
 
 namespace PPMModelLibrary.Models.Properties
 {
