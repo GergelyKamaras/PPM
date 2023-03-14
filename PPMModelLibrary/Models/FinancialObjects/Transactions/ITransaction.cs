f000e6f8931c0aadddc1651aaecfcc350a26ea1b
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-03-14 16:02:33 +0100

    Refactor: remove duplicate properties from interfaces

--- PPMModelLibrary/Models/FinancialObjects/Transactions/ITransaction.cs
+++ PPMModelLibrary/Models/FinancialObjects/Transactions/ITransaction.cs
@@ -1,14 +1,7 @@
-﻿using PPMModelLibrary.Models.Properties;
-
-namespace PPMModelLibrary.Models.FinancialObjects.Transactions
+﻿namespace PPMModelLibrary.Models.FinancialObjects.Transactions
 {
     internal interface ITransaction : IFinancialObject
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public decimal Value { get; set; }
-        public DateTime Date { get; set; }
-        public Property? Property { get; set; }
-        public RentalProperty? RentalProperty { get; set; }
+
     }
 }
--- PPMModelLibrary/Models/FinancialObjects/ValueModifiers/IValueChange.cs
+++ PPMModelLibrary/Models/FinancialObjects/ValueModifiers/IValueChange.cs
@@ -1,14 +1,7 @@
-﻿using PPMModelLibrary.Models.Properties;
-
-namespace PPMModelLibrary.Models.FinancialObjects.ValueModifiers
+﻿namespace PPMModelLibrary.Models.FinancialObjects.ValueModifiers
 {
     internal interface IValueChange : IFinancialObject
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public decimal Value { get; set; }
-        public DateTime Date { get; set; }
-        public Property? Property { get; set; }
-        public RentalProperty? RentalProperty { get; set; }
+        
     }
 }
